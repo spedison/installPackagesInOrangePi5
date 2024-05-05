87899f40673c43d820b37a5725db516fa3f0a758
EDISON RIBEIRO ARAUJO <spedison@users.noreply.github.com>
2024-05-05 17:44:19 -0300

    Update basic_tools.sh

--- basic_tools.sh
+++ basic_tools.sh
@@ -20,9 +20,5 @@ apt update -y
 apt upgrade -y
 
 for program_name in ${PROGRAMS_FOR_INSTALL[@]}; do
-  if ! dpkg -l | grep -q $program_name; then # only install if not already installed
     apt install $program_name -y
-  else
-    echo "[installed] - $program_name"
-  fi
 done
