a98de6f6063107d7688b770491fd75cbe53071f9
EDISON RIBEIRO ARAUJO <spedison@users.noreply.github.com>
2024-05-05 20:23:43 -0300

    Create java_tools.sh

---
+++ java_tools.sh
@@ -0,0 +1,15 @@
+mkdir /media/Disco1T/tools/sdkman
+
+export SDKMAN_DIR="/media/Disco1T/tools/sdkman" && curl -s "https://get.sdkman.io" | bash
+
+source ~/.bashrc
+
+sdk install java 21.0.2-graalce -y
+sdk install java 17.0.8-graalce -n
+sdk install gradle 8.6 -y
+sdk install scala 3.4.1
+sdk install maven 3.9.6
+
+
+
+
