bfb5e36be879149a9446fbd05997a2fa6abda682
EDISON RIBEIRO ARAUJO <spedison@users.noreply.github.com>
2024-05-05 21:45:54 -0300

    Update java_tools.sh

--- java_tools.sh
+++ java_tools.sh
@@ -1,13 +1,26 @@
-
 export SDKMAN_DIR="/media/Disco1T/tools/sdkman" && curl -s "https://get.sdkman.io" | bash
 
 .  ~/.bashrc
 
-sdk install java 21.0.2-graalce -y
-sdk install java 17.0.8-graalce -n
+sdk install java 17.0.8-graalce 
+echo "Y" | sdk install java 21.0.2-graalce 
+echo "n" | sdk install java 22-graal
+
 sdk install gradle 8.6 -y
 sdk install scala 3.4.1
 sdk install maven 3.9.6
+sdk install groovy 4.0.21
+
+export SCALA_HOME=/media/Disco1T/tools/sdkman/candidates/scala/current
+export GRADLE_HOME=/media/Disco1T/tools/sdkman/candidates/gradle/current
+export MVN_HOME=/media/Disco1T/tools/sdkman/candidates/maven/current
+export PATH=$PATH:$JAVA_HOME/bin:$GRADLE_HOME/bin:$SCALA_HOME/bin:$MVN_HOME/bin
+
+cd /media/Disco1T/tools/sdkman/candidates/
+ln -s $PWD/scala/3.4.1  $PWD/scala/current
+ln -s $PWD/gradle/8.6  $PWD/gradle/current
+ln -s $PWD/maven/3.9.6  $PWD/maven/current
+
 
 
 
